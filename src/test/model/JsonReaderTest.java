bf609b2bbe4093fadbc01327b2ce78df0c3c4343
makafuiamouzouvi <hortenseamouzouvi@gmail.com>
2022-08-01 17:59:52 -0700

    Added new Functionality and Added JSON Library2

--- src/test/model/JsonReaderTest.java
+++ src/test/model/JsonReaderTest.java
@@ -1,6 +1,53 @@
 package model;
 
+import org.junit.jupiter.api.Test;
+import persistence.JsonReader;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class JsonReaderTest extends JsonTest{
+
+    @Test
+    void testReaderNonExistentFile() {
+        JsonReader reader = new JsonReader("./data/noSuchFile.json");
+        try {
+            Student student = reader.read();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void testReaderEmptyCourseRoom() {
+        JsonReader reader = new JsonReader("./data/testReaderEmptyWorkRoom.json");
+        try {
+            Student student = reader.read();
+            assertEquals("Makafui Amouzouvi", student.getStudentName());
+            assertEquals(0, student.getCourseRoom().getCourseRoomSize());
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    void testReaderGeneralCourseRoom() {
+        JsonReader reader = new JsonReader("./data/testReaderGeneralStudent.json");
+        try {
+            Student student = reader.read();
+            assertEquals("courses", student.getCourseRoom().getCourseRoomName());
+            List<Course> courses = student.getStudentCourses();
+            assertEquals(2, student.getCourseRoom().getCourseRoomSize());
+            assertEquals("physics",student.getStudentCourses().get(0));
+            assertEquals("statistics",student.getStudentCourses().get(1));
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
 
 
 }
